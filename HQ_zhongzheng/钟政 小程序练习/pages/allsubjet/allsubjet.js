1599d11391ae46479e4d1a0d18df9735d910e6c8
HQ_zhongzheng <1281622446@qq.com>
2020-11-14 11:56:40 +0800

    钟政 小程序练习

---
+++ HQ_zhongzheng/钟政 小程序练习/pages/allsubjet/allsubjet.js
@@ -0,0 +1,117 @@
+// pages/allsubjet/allsubjet.js
+Page({
+  data: {
+   
+  },
+  jump_chinese:function()
+  {
+    wx.redirectTo({
+      url: '/pages/chinese/chinese',
+    })
+  },
+  jump_maths:function()
+  {
+    wx.redirectTo({
+      url: '/pages/maths/maths',
+    })
+  },
+  jump_english:function()
+  {
+    wx.redirectTo({
+      url: '/pages/english/english',
+    })
+  },
+  jump_physics:function()
+  {
+    wx.redirectTo({
+      url: '/pages/physics/physics',
+    })
+  },
+  jump_chemical:function()
+  {
+    wx.redirectTo({
+      url: '/pages/chemical/chemical',
+    })
+  },
+  jump_biology:function()
+  {
+    wx.redirectTo({
+      url: '/pages/biology/biology',
+    })
+  },
+  jump_history:function()
+  {
+    wx.redirectTo({
+      url: '/pages/history/history',
+    })
+  },
+  jump_geography:function()
+  {
+    wx.redirectTo({
+      url: '/pages/geography/geography',
+    })
+  },
+  jump_politics:function()
+  {
+    wx.redirectTo({
+      url: '/pages/politics/politics',
+    })
+  },
+
+    
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad:function(){
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
